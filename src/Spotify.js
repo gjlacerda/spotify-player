d11cf62adf90cd5e00f11c9b2bedaba714484761
gjlacerda <guilherme.lcd@gmail.com>
2018-02-04 16:43:54 -0200

    Importing node-spotify-wrapper library

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'node-spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+    token: 'BQBe_RFQ8EsJdW7fR3lPLPMSx1SUMbe3GrFRTIEWuMNgLz0brgZkaGIVOlIgxXQAndaOGTkEzJD0x7N6mXLYYX_QIcoPNYMrjcRrRmcnzilacpt1oibAnJidr-G8lbxw5T4FMM1Y_n1Wr1U'
+});
+
+export default spotify;
