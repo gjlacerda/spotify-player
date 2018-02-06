f6c4009acf3c1442d7f202eeedf8b89df5e5792f
gjlacerda <guilherme.lcd@gmail.com>
2018-02-06 21:48:41 -0200

    Listing albums

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'node-spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-    token: 'BQBe_RFQ8EsJdW7fR3lPLPMSx1SUMbe3GrFRTIEWuMNgLz0brgZkaGIVOlIgxXQAndaOGTkEzJD0x7N6mXLYYX_QIcoPNYMrjcRrRmcnzilacpt1oibAnJidr-G8lbxw5T4FMM1Y_n1Wr1U'
+    token: 'BQDJEMNJnvkVWJRxXnHabEtZOnIcJ6gEPfgI61bE12utj9O2vf6_OpitiL5kVbV8ztAo5KPVJoHoYwR4ysjxPiCx8m0Be5s0hKwrC4YOuVrw0AsZ-n7mGb3ZmF_whjgrt7SOT1nnKvVgNvQ'
 });
 
 export default spotify;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import spotify from './spotify';
+import renderAlbums from './AlbumList';
+
+const albums = spotify.search.albums('Incubus');
+const $albumList = document.getElementById('album-list');
+
+albums.then(data => renderAlbums(data.albums.items, $albumList));
